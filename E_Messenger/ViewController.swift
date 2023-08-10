4249af8dd0d4194015c4c63d397bced48a531e7d
Justin Zhang <justinzhang321@gmail.com>
2023-08-10 15:58:25 -0700

    Initial Commit

---
+++ E_Messenger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  E_Messenger
+//
+//  Created by Justin Zhang on 8/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
